f9870dedf7cef9d932c866a29b551cfb4a1a8e12
Iossif <iossif123gg@gmail.com>
2023-07-04 07:42:47 +0300

    add test for /

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [ring/ring-jetty-adapter "1.8.2"]] ;; add this line
+                 [ring/ring-jetty-adapter "1.8.2"]
+                 [ring/ring-mock "0.4.0"]]
   :main ^:skip-aot pizza-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- test/pizza_api/core_test.clj
+++ test/pizza_api/core_test.clj
@@ -1,7 +1,10 @@
 (ns pizza-api.core-test
   (:require [clojure.test :refer :all]
+            [ring.mock.request :as mock]
             [pizza-api.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-root-route
+  (testing "GET /"
+    (let [response (app (mock/request :get "/"))]
+      (is (= 200 (:status response)))
+      (is (= "Hello World" (:body response))))))
